563e3f135458ef5124e62a7518b1ba1fc9b64f88
AugustoInAugust <eeuu.augusto.valdez@gmail.com>
2025-06-03 18:30:04 -0400

    create floating-button generic component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { FloatingButtonComponent } from './floating-button/floating-button.component';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, FloatingButtonComponent],
   imports: [CommonModule],
-  exports: [ModalComponent]
+  exports: [ModalComponent, FloatingButtonComponent]
 })
 export class SharedModule { }
